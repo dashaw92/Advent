68648246db194d4cbd3f91d2da3fadb0bc181da8
dashaw92 <dashaw92@gmail.com>
2021-11-15 15:07:19 -0500

    2017 d10p2

--- 2017/day10/src/main.rs
+++ 2017/day10/src/main.rs
@@ -2,40 +2,87 @@ use std::fs::read_to_string;
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let input: Vec<i32> = read_to_string("input.txt")?
+    day1().and(day2())
+}
+
+fn day1() -> Result<()> {
+    let input: Vec<u32> = read_to_string("input.txt")?
         .split(",")
-        .filter_map(|val| val.parse::<i32>().ok())
+        .filter_map(|val| val.parse::<u32>().ok())
         .collect();
 
-    let mut seq: Vec<i32> = (0..256).collect();
+    let mut seq: Vec<u32> = (0..256).collect();
+    let mut params = Params::new();
 
-    twist(&mut seq, &input);
+    twist(&mut seq, &input, &mut params);
     println!("Day 1: {}", seq[0] * seq[1]);
+
     Ok(())
 }
 
-fn twist(seq: &mut Vec<i32>, twists: &[i32]) {
-    let mut skip = 0;
-    let mut cursor = 0usize;
+fn day2() -> Result<()> {
+    let mut input: Vec<u32> = read_to_string("input.txt")?
+        .chars()
+        .map(Into::into)
+        .collect();
 
+    input.extend_from_slice(&[17, 31, 73, 47, 23]);
+
+    let mut seq: Vec<u32> = (0..256).collect();
+
+    let mut params = Params::new();
+    for _iter in 0..64 {
+        twist(&mut seq, &input, &mut params);
+    }
+
+    let mut dense: [u32; 16] = [0; 16];
+    let chunks: Vec<&[u32]> = seq.chunks(16).collect();
+    for idx in 0..chunks.len() {
+        dense[idx] = chunks[idx].iter()
+            .fold(0, |acc, val| acc ^ val);
+    }
+
+    let hash = format!("{:x?}", dense).replace("[", "").replace("]", "").replace(", ", "");
+    println!("{}", hash);
+    Ok(())
+}
+
+fn twist(seq: &mut Vec<u32>, twists: &[u32], params: &mut Params) {
     for &len in twists {
-        let end_actual = len as usize + cursor;
+        let end_actual = len as usize + params.cursor;
         
         let mut current = Vec::new();
-        for idx in cursor..end_actual {
+        for idx in params.cursor..end_actual {
             let idx_wrapped = idx % seq.len();
             current.push(seq[idx_wrapped]);
         }
 
         current.reverse();
         for val in current {
-            seq[cursor] = val;
-            cursor += 1;
-            cursor %= seq.len();
+            params.cursor %= seq.len();
+            seq[params.cursor] = val;
+            params.cursor += 1;
         }
 
-        cursor += skip;
-        skip += 1;
+        params.finalize_iteration()
+    }
+
+}
+
+struct Params {
+    cursor: usize,
+    skip: usize,
+}
+
+impl Params {
+    
+    fn new() -> Self {
+        Params { cursor: 0, skip: 0 }
+    }
+
+    fn finalize_iteration(&mut self) {
+        self.cursor += self.skip;
+        self.skip += 1;
     }
 
 }
