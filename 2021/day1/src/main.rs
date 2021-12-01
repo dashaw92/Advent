5bc9cce8224b2e423ed5b643b53f3a879d2de5f6
dashaw92 <dashaw92@gmail.com>
2021-12-01 17:49:03 -0500

    2021 Day 1 complete!

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -7,15 +7,27 @@ fn main() -> Result<(), Box<dyn Error>> {
         .filter_map(|line| line.parse().ok())
         .collect();
 
-    println!("Day 1: {} scans were greater than their previous entry!", solve(&input));
+    println!("Day 1: {} scans were greater than their previous entry!", solve(&input, 1));
+    println!("Day 2: {} scan groups were greater than the previous group!", solve(&input, 3));
 
     Ok(())
 }
 
-pub fn solve(input: &[usize]) -> usize {
-    input.iter()
-        .zip(input.iter().skip(1))
-        .filter(|(&first, &second)| second > first)
+pub fn solve(input: &[usize], window_size: usize) -> usize {
+    fn sum(slice: &[usize]) -> usize {
+        slice
+            .iter()
+            .sum()
+    }
+
+    input
+        .windows(window_size)
+        .zip(input[1..].windows(window_size))
+        .inspect(|(_first, _second)| {
+            #[cfg(test)] //only print debug output in test configuration
+            eprintln!("first: Σ{:?} = {}\nsecond: Σ{:?} = {}", _first, sum(_first), _second, sum(_second));
+        })
+        .filter(|(first, second)| sum(second) > sum(first))
         .count()
 }
 
@@ -23,20 +35,25 @@ pub fn solve(input: &[usize]) -> usize {
 mod test {
     use super::*;
 
-    #[test]
-    fn provided() {
-        let input = [199,
-            200,
-            208,
-            210,
-            200,
-            207,
-            240,
-            269,
-            260,
-            263,
-        ];
+    const PROVIDED_INPUT: [usize; 10] = [199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263,
+    ];
 
-        assert_eq!(7, solve(&input))
+    #[test]
+    fn provided_p1() {
+        assert_eq!(7, solve(&PROVIDED_INPUT, 1))
+    }
+
+    #[test]
+    fn provided_p2() {
+        assert_eq!(5, solve(&PROVIDED_INPUT, 3))
     }
 }
