f53c5ca1067c3c28044b88474afff36483210f83
dashaw92 <dashaw92@gmail.com>
2021-12-02 00:44:32 -0500

    2021 Day 2 Part 2!

--- 2021/day2/src/main.rs
+++ 2021/day2/src/main.rs
@@ -1,17 +1,18 @@
 use std::error::Error;
 use std::fs::read_to_string;
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let input: Vec<String> = read_to_string("input.txt")?
         .lines()
         .map(ToOwned::to_owned)
         .collect();
 
-    println!("Day 1: Horizontal * Depth = {}", solve(&input));
+    println!("Day 1: Horizontal * Depth = {}", solve_p1(&input));
+    println!("Day 2: Horizontal * Depth = {}", solve_p2(&input));
     Ok(())
 }
 
-fn solve(input: &[impl AsRef<str>]) -> isize {
+fn solve_p1(input: &[impl AsRef<str>]) -> isize {
     let mut depth = 0;
     let mut pos = 0;
 
@@ -25,6 +26,31 @@ fn solve(input: &[impl AsRef<str>]) -> isize {
             "forward" => pos += amount,
             "down" => depth += amount,
             "up" => depth -= amount,
+            _ => continue,
+        }
+    }
+
+    depth * pos
+}
+
+fn solve_p2(input: &[impl AsRef<str>]) -> isize {
+    let mut depth = 0;
+    let mut pos = 0;
+    let mut aim = 0;
+
+    for directive in input {
+        let mut parts = directive.as_ref().split(" ");
+
+        let command = parts.nth(0).unwrap();
+        let amount: isize = parts.nth(0).and_then(|amt| amt.parse().ok()).unwrap();
+
+        match command {
+            "forward" => {
+                pos += amount;
+                depth += aim * amount;
+            }
+            "down" => aim += amount,
+            "up" => aim -= amount,
             _ => continue,
         }
     }
@@ -47,6 +73,11 @@ mod test {
 
     #[test]
     fn provided_p1() {
-        assert_eq!(150, solve(&PROVIDED))
+        assert_eq!(150, solve_p1(&PROVIDED))
+    }
+
+    #[test]
+    fn provided_p2() {
+        assert_eq!(900, solve_p2(&PROVIDED))
     }
 }
