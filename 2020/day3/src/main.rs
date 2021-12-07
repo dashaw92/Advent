3fe3063d6e88e81ef018e0c8cd2bdafaf160a74b
dashaw92 <dashaw92@gmail.com>
2021-12-07 05:08:22 -0500

    2020 Day 3 Part 2

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -4,11 +4,12 @@ use std::fs::read_to_string;
 fn main() -> Result<(), Box<dyn Error>> {
     let input = read_to_string("input.txt")?;
 
-    println!("Part 1: Hit {} trees!", solve(&input));
+    println!("Part 1: Hit {} trees!", solve(&input, (3, 1)));
+    println!("Part 2: Hit {} trees!", solve_p2(&input));
     Ok(())
 }
 
-fn solve(input: impl AsRef<str>) -> usize {
+fn solve(input: impl AsRef<str>, slope: (usize, usize)) -> usize {
     let input: Vec<&str> = input.as_ref().lines().collect();
 
     let mut pos: (usize, usize) = (0, 0);
@@ -19,15 +20,25 @@ fn solve(input: impl AsRef<str>) -> usize {
             count += 1;
         }
 
-        if pos.1 + 1 >= input.len() {
+        pos.1 += slope.1;
+        if pos.1 >= input.len() {
             break;
         }
 
-        pos.1 += 1;
-        pos.0 = (pos.0 + 3) % input[pos.1].len();
+        pos.0 = (pos.0 + slope.0) % input[pos.1].len();
     }
 
     count
+}
+
+fn solve_p2(input: impl AsRef<str>) -> usize {
+    [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ].into_iter().map(|slope| solve(&input, slope)).product()
 }
 
 #[cfg(test)]
