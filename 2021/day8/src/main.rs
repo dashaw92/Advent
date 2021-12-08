31deaa078f47de84894be608444f92a0de1e4f15
dashaw92 <dashaw92@gmail.com>
2021-12-08 00:09:05 -0500

    2021 Day 8 Part 1

--- 2021/day8/src/main.rs
+++ 2021/day8/src/main.rs
@@ -9,7 +9,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn solve_p1(input: impl AsRef<str>) -> usize {
-    0
+    let outputs: Vec<&str> = input.as_ref().lines()
+        .inspect(|s| println!("{}", s))
+        .filter_map(|s| s.split("|").nth(1))
+        .map(str::trim)
+        .flat_map(|s| s.split_ascii_whitespace())
+        .filter(|&s| {
+            [2, 3, 4, 7].contains(&s.len())
+        })
+        .collect();
+
+    outputs.len()
 }
 
 fn solve_p2(input: impl  AsRef<str>) -> usize {
@@ -21,11 +31,20 @@ mod test {
 
     use super::*;
 
-    const PROVIDED: &str = "";
+    const PROVIDED: &str = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
 
     #[test]
     fn provided_p1() {
-        assert_eq!(0, solve_p1(PROVIDED));
+        assert_eq!(26, solve_p1(PROVIDED));
     }
 
     #[test]
