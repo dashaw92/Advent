5383b30fd0f2d5bc6e79722892ec11887fd56007
dashaw92 <dashaw92@gmail.com>
2022-11-05 10:42:11 -0400

    2016 d6p2

--- 2016/day6/src/main.rs
+++ 2016/day6/src/main.rs
@@ -5,13 +5,17 @@ use std::fs::read_to_string;
 fn main() -> Result<(), Box<dyn Error>> {
     let input = read_to_string("input.txt")?;
 
-    println!("Part 1: {}", solve_p1(&input));
+    let (p1, p2) = solve(&input);
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
     Ok(())
 }
 
-fn solve_p1(input: impl AsRef<str>) -> String {
+type FreqMap = Vec<HashMap<char, u32>>;
+
+fn build_freqs(input: impl AsRef<str>) -> FreqMap {
     let input: Vec<&str> = input.as_ref().lines().collect();
-    let mut freq: Vec<HashMap<char, i32>> = Vec::with_capacity(input.len());
+    let mut freq: FreqMap = Vec::with_capacity(input.len());
     for _ in 0..input[0].len() {
         freq.push(HashMap::new());
     }
@@ -22,10 +26,24 @@ fn solve_p1(input: impl AsRef<str>) -> String {
         }
     }
 
-    freq.iter()
+    freq
+}
+
+fn solve(input: impl AsRef<str>) -> (String, String) {
+    let freq = build_freqs(input);
+
+    let p1 = freq
+        .iter()
         .map(|map| map.iter().max_by_key(|(_, count)| **count).unwrap())
         .map(|(c, _)| *c)
-        .collect()
+        .collect();
+    let p2 = freq
+        .iter()
+        .map(|map| map.iter().min_by_key(|(_, count)| **count).unwrap())
+        .map(|(c, _)| *c)
+        .collect();
+
+    (p1, p2)
 }
 
 #[cfg(test)]
@@ -52,6 +70,6 @@ enarar";
 
     #[test]
     fn provided_p1() {
-        assert_eq!("easter", &solve_p1(PROVIDED));
+        assert_eq!(("easter".to_owned(), "advent".to_owned()), solve(PROVIDED));
     }
 }
