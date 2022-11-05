2fbaf773be2ed35a2d82282c18675e4abfd9e223
dashaw92 <dashaw92@gmail.com>
2022-11-05 10:21:57 -0400

    2016 d5p2

--- 2016/day5/src/main.rs
+++ 2016/day5/src/main.rs
@@ -6,6 +6,8 @@ fn main() {
         .expect("Expected puzzle input as args[0]");
 
     println!("Part 1: {}", solve_p1(&input));
+    println!("\nOk, prepare the popcorn... p2 will take a long time");
+    println!("Part 2: {}", solve_p2(&input));
 }
 
 fn solve_p1(input: impl AsRef<str>) -> String {
@@ -16,7 +18,45 @@ fn solve_p1(input: impl AsRef<str>) -> String {
         .map(compute)
         .map(|d| format!("{d:x}"))
         .filter(|digest| digest.starts_with("00000"))
+        .inspect(|hex| println!("p1: found hash {hex}"))
         .map(|slice| slice.chars().nth(5).unwrap())
         .take(8)
         .collect()
+}
+
+fn solve_p2(input: impl AsRef<str>) -> String {
+    let mut found = 0;
+    let seed = input.as_ref();
+    let mut code: [u8; 8] = [b'-'; 8];
+
+    for i in 0.. {
+        if found == 8 {
+            break;
+        }
+
+        let digest = compute(format!("{seed}{i}"));
+        let hex = format!("{digest:x}");
+
+        if hex.starts_with("00000") {
+            let pos = hex.chars().nth(5).and_then(|c| c.to_digit(10));
+            //invalid pos, ignore this hash as per spec
+            //pos already found, ignore this hash
+            let pos = match pos {
+                Some(p) => {
+                    if p > 7 || code[p as usize] != b'-' {
+                        continue;
+                    }
+                    p as usize
+                }
+                None => continue,
+            };
+
+            let val = hex.chars().nth(6).unwrap() as u8;
+            code[pos] = val;
+            found += 1;
+            println!("p2::[{i}] Found {hex}! Remaining: {}", code.len() - found);
+        }
+    }
+
+    String::from_utf8(code.to_vec()).unwrap()
 }
