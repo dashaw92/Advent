492748c294cb93005b7a91acaa69584f12452ecc
dashaw92 <dashaw92@gmail.com>
2022-12-06 14:51:40 -0500

    2022 Day 6 Part 2

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -13,22 +13,28 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn solve(input: impl AsRef<str>) -> (usize, usize) {
     let chars: Vec<char> = input.as_ref().chars().collect();
 
-    let p1 = chars
-        .windows(4)
-        .enumerate()
-        .find(|(_, seq)| is_packet_marker(seq))
-        .map(|(i, _)| i + 4)
-        .unwrap();
-    (p1, 0)
+    let p1 = find(&chars, 4);
+    let p2 = find(&chars, 14);
+    (p1, p2)
 }
 
-fn is_packet_marker(seq: &[char]) -> bool {
+fn find(chars: &[char], length: usize) -> usize {
+    chars
+        .windows(length)
+        .enumerate()
+        .find(|(_, seq)| is_marker(seq, length))
+        .map(|(i, _)| i + length)
+        .unwrap()
+}
+
+fn is_marker(seq: &[char], length: usize) -> bool {
     let mut bits: u32 = 0;
     seq.iter()
         .map(|&ch| (ch as u8) - b'a')
         .for_each(|bit| bits |= 1 << bit);
 
-    bits.count_ones() == 4
+    // println!("{seq:?} {bits:032b}");
+    bits.count_ones() == length as u32
 }
 
 #[cfg(test)]
@@ -36,18 +42,18 @@ mod test {
 
     use super::*;
 
-    const PROVIDED: [(&str, usize); 5] = [
-        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    const PROVIDED: [(&str, (usize, usize)); 5] = [
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", (7, 19)),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", (5, 23)),
+        ("nppdvjthqldpwncqszvftbrmjlhg", (6, 23)),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", (10, 29)),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", (11, 26)),
     ];
 
     #[test]
     fn provided_p1() {
         PROVIDED
             .into_iter()
-            .for_each(|test| assert_eq!(solve(test.0).0, test.1));
+            .for_each(|test| assert_eq!(solve(test.0), test.1));
     }
 }
