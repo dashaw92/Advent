38d08cd13a871790d770a5fb2334d1bcc7bc61bb
dashaw92 <dashaw92@gmail.com>
2023-08-25 15:30:03 -0400

    2015 D1 F#

---
+++ fs/2015/day1.fsx
@@ -0,0 +1,57 @@
+(*
+    Author: dashaw92
+    8/25/2023
+
+    Advent of Code 2015 Day 1
+*)
+
+//Read the input and flatten it to a single string
+let input = 
+    System.IO.File.ReadLines($"{__SOURCE_DIRECTORY__}/day1.txt") 
+    |> Seq.toList
+    |> String.concat " "
+
+let stairVal stair =
+    match stair with
+    | '(' -> 1
+    | ')' -> -1
+    | _ -> failwith "Bad input"
+
+//Parsed form of the input, where all "stairs" are converted to int deltas
+//to be applied to the current stair
+let mappedInput =
+    input |> Seq.map stairVal |> List.ofSeq
+
+//Original part1. Not needed
+// let part1 =
+//     mappedInput
+//     |> Seq.sum
+
+//My strategy for solving this is simple. For every index i of the input,
+//get the sum of the sublist of all indices from start to i set the current
+//value in the list to the sum.
+//By doing this, we get an output list where each element corresponds to Santa's
+//stair level at that index
+let sums input =
+    let buildSums idx _ =
+        input
+        |> List.take (idx + 1)
+        |> List.sum
+    input
+    |> List.mapi buildSums
+
+//P2 cares about when we first reach level -1
+let part2Pred elem = elem = -1
+
+//Finally, get the solutions for both parts
+let solvedInput = 
+    mappedInput
+    |> sums
+
+//The last element is Santa's final position
+let part1 = solvedInput |> List.last
+let part2 =
+    (solvedInput
+    |> List.findIndex part2Pred) + 1
+
+printfn $"Part 1: {part1} | Part 2: {part2}"
