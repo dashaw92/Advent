b77e8664e5ba006ede4f3c5037e290aca9ae36a7
dashaw92 <dashaw92@gmail.com>
2023-12-01 09:39:58 -0500

    Yeah I don't know

--- fs/2023/day1.fsx
+++ fs/2023/day1.fsx
@@ -37,45 +37,44 @@ let findDigit (str: string) =
 let solveP1 = List.map findDigit >> List.sum
 let p1 = solveP1 input
 
-// let numbers = Map [
-//     ("one", 1)
-//     ("two", 2)
-//     ("three", 3)
-//     ("four", 4)
-//     ("five", 5)
-//     ("six", 6)
-//     ("seven", 7)
-//     ("eight", 8)
-//     ("nine", 9)
-//     ("zero", 0)
-// ]
+let numbers = Map [
+    ("one", 1)
+    ("two", 2)
+    ("three", 3)
+    ("four", 4)
+    ("five", 5)
+    ("six", 6)
+    ("seven", 7)
+    ("eight", 8)
+    ("nine", 9)
+]
 
-// let isPartialMatch (str: string) =
-//     numbers.Keys
-//     |> Seq.exists (fun (key: string) -> key.StartsWith str)
+let isPartialMatch (str: string) =
+    numbers.Keys
+    |> Seq.exists (fun (key: string) -> key.StartsWith str)
 
-// let exactMatch (str: string) = numbers.TryFind str
+let exactMatch (str: string) = numbers.TryFind str
 
-// let findDigit2 (str: string) =
-//     let folder (partial, acc) nextChar =
-//         match nextChar with
-//         | digit when Char.IsAsciiDigit digit -> ([], charToInt digit :: acc)
-//         | letter ->
-//             let next = letter :: partial
-//             let maybeNumber = next |> List.rev |> String.Concat
-//             if isPartialMatch maybeNumber then
-//                 match exactMatch maybeNumber with
-//                 | Some value -> ([], value :: acc)
-//                 | None -> (next, acc)
-//             else
-//                 ([letter], acc)
+let findDigit2 (str: string) =
+    let folder (partial, acc) nextChar =
+        match nextChar with
+        | digit when Char.IsAsciiDigit digit -> ([], charToInt digit :: acc)
+        | letter ->
+            let next = letter :: partial
+            let maybeNumber = next |> List.rev |> String.Concat
+            if isPartialMatch maybeNumber then
+                match exactMatch maybeNumber with
+                | Some value -> ([letter], value :: acc)
+                | None -> (next, acc)
+            else
+                ([letter], acc)
 
-//     str.ToCharArray ()
-//     |> List.ofArray
-//     |> List.fold folder ([], [])
-//     |> snd
-//     |> (fun ints -> if ints.Length = 1 then 0, 0 else List.head ints, List.last ints)
-//     |> tupToInt
+    str.ToCharArray ()
+    |> List.ofArray
+    |> List.fold folder ([], [])
+    |> snd
+    |> (fun ints -> List.head ints, List.last ints)
+    |> tupToInt
 
-// let solveP2 = List.map findDigit2 >> List.sum
-// let p2 = solveP2 input
+let solveP2 = List.map findDigit2 >> List.sum
+let p2 = solveP2 input
