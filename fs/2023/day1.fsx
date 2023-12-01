c0328155f735cdb418c31043ecf6d74c96d7e4cd
dashaw92 <dashaw92@gmail.com>
2023-12-01 16:49:16 -0500

    Clean up p1

--- fs/2023/day1.fsx
+++ fs/2023/day1.fsx
@@ -10,71 +10,14 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet".Split '\n' |> List.ofArray
 
-let exampleInput2 =
-    "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen".Split '\n' |> List.ofArray
-
-let charToInt = Char.GetNumericValue >> int
-let tupToInt (b, a) = a * 10 + b
-
 let findDigit (str: string) =
-    let folder acc nextChar  =
-        match nextChar with
-        | digit when Char.IsAsciiDigit digit -> charToInt digit :: acc
-        | _ -> acc
-    str.ToCharArray ()
-    |> List.ofArray
-    |> List.fold folder []
-    |> (fun ints -> List.head ints, List.last ints)
-    |> tupToInt
-
+    let charToInt = Char.GetNumericValue >> int
+    let tupToInt (a, b) = a * 10 + b
+    let ints = 
+        str.ToCharArray ()
+        |> Array.filter Char.IsAsciiDigit
+        |> Array.map charToInt
+    tupToInt (Array.head ints, Array.last ints)
 
 let solveP1 = List.map findDigit >> List.sum
 let p1 = solveP1 input
-
-let numbers = Map [
-    ("one", 1)
-    ("two", 2)
-    ("three", 3)
-    ("four", 4)
-    ("five", 5)
-    ("six", 6)
-    ("seven", 7)
-    ("eight", 8)
-    ("nine", 9)
-]
-
-let isPartialMatch (str: string) =
-    numbers.Keys
-    |> Seq.exists (fun (key: string) -> key.StartsWith str)
-
-let exactMatch (str: string) = numbers.TryFind str
-
-let findDigit2 (str: string) =
-    let folder (partial, acc) nextChar =
-        match nextChar with
-        | digit when Char.IsAsciiDigit digit -> ([], charToInt digit :: acc)
-        | letter ->
-            let next = letter :: partial
-            let maybeNumber = next |> List.rev |> String.Concat
-            if isPartialMatch maybeNumber then
-                match exactMatch maybeNumber with
-                | Some value -> ([letter], value :: acc)
-                | None -> (next, acc)
-            else
-                ([letter], acc)
-
-    str.ToCharArray ()
-    |> List.ofArray
-    |> List.fold folder ([], [])
-    |> snd
-    |> (fun ints -> List.head ints, List.last ints)
-    |> tupToInt
-
-let solveP2 = List.map findDigit2 >> List.sum
-let p2 = solveP2 input
