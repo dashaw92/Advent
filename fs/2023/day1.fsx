8fad2869e4427c123e02825a54d5257d38daccb0
dashaw92 <dashaw92@gmail.com>
2023-12-02 08:54:41 -0500

    Finish day 1 p2

--- fs/2023/day1.fsx
+++ fs/2023/day1.fsx
@@ -4,20 +4,27 @@ let input =
     System.IO.File.ReadLines($"{__SOURCE_DIRECTORY__}/day1.txt") 
     |> Seq.toList
 
-let exampleInput =
-    "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet".Split '\n' |> List.ofArray
-
 let findDigit (str: string) =
     let charToInt = Char.GetNumericValue >> int
-    let tupToInt (a, b) = a * 10 + b
-    let ints = 
-        str.ToCharArray ()
-        |> Array.filter Char.IsAsciiDigit
-        |> Array.map charToInt
-    tupToInt (Array.head ints, Array.last ints)
+    
+    str.ToCharArray ()
+    |> Array.filter Char.IsAsciiDigit
+    |> Array.map charToInt
+    |> (fun ints -> Array.head ints * 10 + Array.last ints)
+
+let filterP2 (input: string) =
+    let replace (a: string) (b: string) (str: string) = str.Replace (a, b)
+    replace "one" "o1e" input
+    |> replace "two" "t2o"
+    |> replace "three" "t3e"
+    |> replace "four" "f4r"
+    |> replace "five" "f5e"
+    |> replace "six" "s6x"
+    |> replace "seven" "s7n"
+    |> replace "eight" "e8t"
+    |> replace "nine" "n9e"
 
 let solveP1 = List.map findDigit >> List.sum
+let solveP2 = List.map (filterP2 >> findDigit) >> List.sum
 let p1 = solveP1 input
+let p2 = solveP2 input
