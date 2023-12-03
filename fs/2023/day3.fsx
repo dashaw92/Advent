20f309c532dc4b37b40dfda4f1a79a7fd98a94fa
dashaw92 <dashaw92@gmail.com>
2023-12-03 08:24:40 -0500

    2023 Day 3 Part 2

--- fs/2023/day3.fsx
+++ fs/2023/day3.fsx
@@ -1,45 +1,48 @@
 open System
 
-type Schem = {
-    Grid: char array
-    Width: int
-    Height: int
-}
+type Schem =
+    { Grid: char array
+      Width: int
+      Height: int }
 
 type Part = { X: int; Y: int; Value: int }
 
-let parseSchem (str: string) = 
+let parseSchem (str: string) =
     let lines = str.Split '\n'
 
     let width = lines[0].Length
     let height = lines.Length
-    let grid = (str.Replace ("\n", "")).ToCharArray ()
+    let grid = (str.Replace("\n", "")).ToCharArray()
 
-    { Grid = grid; Width = width; Height = height }
+    { Grid = grid
+      Width = width
+      Height = height }
 
-let isSymbol ch = ch <> '.' && not <| Char.IsAsciiDigit ch
+let isSymbol ch =
+    ch <> '.' && not <| Char.IsAsciiDigit ch
+
+let isGear = (=) '*'
 let isPart = Char.IsAsciiDigit
 
-let findSymbols schem =
+let findSymbols filter schem =
     schem.Grid
     |> Array.indexed
     |> List.ofArray
-    |> List.filter (snd >> isSymbol)
+    |> List.filter (snd >> filter)
     |> List.map (fun (idx, _) -> idx % schem.Width, idx / schem.Width)
 
 let neighbors (x, y) =
-    [
-        x - 1, y - 1
-        x - 1, y
-        x - 1, y + 1
-        x    , y - 1
-        x    , y + 1
-        x + 1, y - 1
-        x + 1, y
-        x + 1, y + 1
-    ]
+    [ x - 1, y - 1
+      x - 1, y
+      x - 1, y + 1
+      x, y - 1
+      x, y + 1
+      x + 1, y - 1
+      x + 1, y
+      x + 1, y + 1 ]
 
-let inBounds schem (x, y) = x >= 0 && x < schem.Width && y >= 0 && y < schem.Height
+let inBounds schem (x, y) =
+    x >= 0 && x < schem.Width && y >= 0 && y < schem.Height
 
 let idx schem (x, y) = schem.Grid[y * schem.Width + x]
 
@@ -53,39 +56,42 @@ let partAt schem (x, y) =
     let startIdx = findIdx ((+) -1) (x, y)
     let endIdx = findIdx ((+) 1) (x, y)
 
-    let number = schem.Grid[(y * schem.Width + startIdx) .. (y * schem.Width + endIdx)] |> String.Concat |> int
+    let number =
+        schem.Grid[(y * schem.Width + startIdx) .. (y * schem.Width + endIdx)]
+        |> String.Concat
+        |> int
+
     { X = startIdx; Y = y; Value = number }
 
-let findParts schem =
-
-    findSymbols schem
+let collectParts filter schem =
+    findSymbols filter schem
     |> List.map neighbors
     |> List.map (List.filter (inBounds schem))
     |> List.map (List.filter (idx schem >> isPart))
+
+let findParts schem =
+    collectParts isSymbol schem
     |> List.fold (List.append) []
     |> List.map (partAt schem)
     |> List.distinctBy (fun part -> (part.X, part.Y))
 
-let solveP1 =
+let findGears schem =
+    collectParts isGear schem
+    |> List.map (List.map (partAt schem))
+    |> List.map (List.distinctBy (fun part -> (part.X, part.Y)))
+    |> List.filter (List.length >> (=) 2)
+
+let solveP1 = parseSchem >> findParts >> List.map _.Value >> List.sum
+
+let solveP2 =
     parseSchem
-    >> findParts
-    >> List.map _.Value
+    >> findGears
+    >> List.map (List.map _.Value)
+    >> List.map (List.reduce (*))
     >> List.sum
 
-let example =
-    "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598.."
-
-let input = 
-    IO.File.ReadLines($"{__SOURCE_DIRECTORY__}/day3.txt")
-    |> String.concat "\n"
+let input =
+    IO.File.ReadLines($"{__SOURCE_DIRECTORY__}/day3.txt") |> String.concat "\n"
 
 let p1 = solveP1 input
+let p2 = solveP2 input
