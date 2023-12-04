cade7a396150d5cda83a2c85ee7f51c0b9827a7b
dashaw92 <dashaw92@gmail.com>
2023-12-04 09:15:11 -0500

    2023 Day 4 Part 2

--- fs/2023/day4.fsx
+++ fs/2023/day4.fsx
@@ -1,37 +1,72 @@
 open System
+open System.Text.RegularExpressions
+
+type Card = { Id: int; Matches: int; Points: int }
+
+let removeExcessSpaces (line: string) = Regex.Replace (line, " {2,}", " ")
 
 let toNums (numbers: string) =
     numbers.Split " "
     |> Array.map (fun str -> str.Trim ())
     |> Array.filter (not << String.IsNullOrEmpty)
     |> Array.map int
-    |> List.ofArray
+    |> List.ofArray    
 
-let points (card: string) =
-    let sp = card.Split " | "
-    let winning = toNums (sp[0].Split ": ").[1]
-    let numbers = toNums sp[1]
-
+let points winning numbers =
     let power = 
         numbers
         |> List.filter (fun number -> List.contains number winning)
         |> List.length
 
     match power with
-    | x when x > 0 -> pown 2 (x - 1)
-    | _ -> 0
+    | x when x > 0 -> x, pown 2 (x - 1)
+    | _ -> 0, 0
 
-let example =
-    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".Split '\n' |> List.ofArray
+let parseCard (card: string) =
+    let card = removeExcessSpaces card
+    let sp = card.Split " | "
+    let id = ((sp[0].Split " ").[1].Split ":").[0] |> int
+    let winning = toNums (sp[0].Split ": ").[1]
+    let numbers = toNums sp[1]
+
+    let (matches, points) = points winning numbers
+    { Id = id; Matches = matches; Points = points }
+
+let getOr0 m k = defaultArg (Map.tryFind k m) 0
+let insertOrChange m (vFn: int -> int) k =
+    let v = getOr0 m k
+    Map.add k (vFn v) m
+
+
+let merge (m1: Map<'a, int>) (m2: Map<'a, int>) =
+    let m1k = m1.Keys |> List.ofSeq
+    let m2k = m2.Keys |> List.ofSeq
+
+    List.concat (seq [ m1k; m2k ])
+    |> List.map (fun cardId -> cardId, max (getOr0 m1 cardId) (getOr0 m2 cardId))
+    |> Map
+
+let processP2 cards =
+    let reduce acc card =
+        let current = card.Matches
+        let amount = getOr0 acc card.Id
+        if current <> 0 then
+            [card.Id + 1 .. card.Id + current]
+            |> List.map (insertOrChange acc ((+) amount))
+            |> List.reduce merge
+        else
+            acc
+
+    let originalMap = List.map (fun x -> x.Id, 1) cards |> Map
+    List.fold reduce originalMap cards
+    |> Map.values
+    |> Seq.sum
+
+let solveP1 = List.map parseCard >> List.map _.Points >> List.sum
+let solveP2 = List.map parseCard >> processP2
 
 let input =
     IO.File.ReadLines($"{__SOURCE_DIRECTORY__}/day4.txt") |> List.ofSeq
 
-let solveP1 = List.map points >> List.sum
-
 let p1 = solveP1 input
+let p2 = solveP2 input
