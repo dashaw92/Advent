30e3d1a96ca19f2839a43f6f6d3319f8cbb3c272
dashaw92 <dashaw92@gmail.com>
2023-12-06 16:28:44 -0500

    2023 Day 6 Part 2

--- fs/2023/day6.fsx
+++ fs/2023/day6.fsx
@@ -3,29 +3,40 @@
 open AoCShared
 open System
 
-type Race = { Time: int; Record: int }
+type Race = { Time: int64; Record: int64 }
 let race (time, record) = { Time = time; Record = record }
 
 let parseInput input =
     (despace input).Split '\n'
     |> Array.map (fun line -> (line.Split ' ')[1..])
-    |> Array.map (Array.map int)
+    |> Array.map (Array.map int64)
     |> (fun arrays -> Array.zip arrays[0] arrays[1] |> Array.map race)
 
+let combineRaces input =
+    (despace input).Split '\n'
+    |> Array.map (fun line -> (line.Split ' ')[1..])
+    |> Array.map (String.Concat)
+    |> Array.map int64
+    |> (fun values -> { Time = values[0]; Record = values[1] })
+    |> Array.singleton
+
 let allMethods race =
-    let min = 1
-    let max = race.Time - 1
+    let min = 1L
+    let max = race.Time - 1L
     seq {
         for i in min..max do
             let speed = i
             yield i * (race.Time - i)
     } |> Seq.filter (fun method -> method > race.Record)
 
-let solveP1 =
-    parseInput
+let solve parser =
+    parser
     >> Array.map allMethods
     >> Array.map (Seq.length)
     >> Array.reduce ( * )
+
+let solveP1 = solve parseInput
+let solveP2 = solve combineRaces
 
 let input = rf "day6.txt"
 let example =
