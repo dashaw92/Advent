c8ecfa6227aa682e05627f91338e46a03eb2262c
dashaw92 <dashaw92@gmail.com>
2023-12-09 20:01:44 -0500

    Move inputs to own folder to clean up ws

--- fs/2023/AoCShared.fsx
+++ fs/2023/AoCShared.fsx
@@ -3,4 +3,4 @@ open System.Text.RegularExpressions
 
 let despace (line: string) = Regex.Replace (line, " {2,}", " ")
 
-let rf path = IO.File.ReadLines($"{__SOURCE_DIRECTORY__}/{path}") |> String.concat "\n"
+let rf path = IO.File.ReadLines($"{__SOURCE_DIRECTORY__}/inputs/{path}") |> String.concat "\n"
