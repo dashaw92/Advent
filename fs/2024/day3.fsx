2f4c5e5ba7f90e16947cf9067688f09ab3f38863
dashaw92 <dashaw92@gmail.com>
2024-12-03 16:20:22 -0500

    2024 Day 3 Part 2

--- fs/2024/day3.fsx
+++ fs/2024/day3.fsx
@@ -13,16 +13,67 @@ let cleanNums (str: string) =
 
 let input = rf "day3.txt"
 let input2 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+let input3 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
-let re = Regex("mul\((\d+),(\d+)\)", RegexOptions.Compiled)
+let findMuls = Regex("mul\((\d+),(\d+)\)", RegexOptions.Compiled)
+let findDos = Regex("do\(\)", RegexOptions.Compiled)
+let findDonts = Regex("don't\(\)", RegexOptions.Compiled)
 
-let allMatches (re: Regex) str =
+let allMuls str =
+    findMuls.Matches(str)
+    |> Seq.map (fun m -> m.Index, int m.Groups[1].Value, int m.Groups[2].Value)
+
+let getIndexesOf (re: Regex) str =
     re.Matches(str)
-    |> Seq.map (fun m -> int m.Groups[1].Value, int m.Groups[2].Value)
+    |> Seq.map (fun m -> m.Index)
+
+let idxAndTotal input =
+    allMuls input
+    |> Seq.map (fun (idx, a, b) -> idx, a * b)
 
 let p1 =
-    allMatches re input
-    |> Seq.map (fun (a, b) -> a * b)
+    idxAndTotal input
+    |> Seq.map snd
     |> Seq.sum
 
+
 printfn $"%A{p1}"
+
+type P2 =
+| Do of int
+| Dont of int
+
+let allDosAndDonts input =
+    let dos = 
+        getIndexesOf findDos input 
+        |> List.ofSeq |> List.map Do
+    let donts = 
+        getIndexesOf findDonts input 
+        |> List.ofSeq |> List.map Dont
+    
+    List.append dos donts
+    |> List.append [Do 0]
+    |> List.sortBy (
+        function
+        | Do a -> a
+        | Dont a -> a
+    )
+    |> List.rev
+
+let valueOf =
+    function
+    | Do x -> x
+    | Dont x -> x
+
+let shouldMul dosDonts idx =
+    match List.find (fun flag -> idx > valueOf flag) dosDonts with
+    | Do _ -> true
+    | Dont _ -> false
+
+let p2 =
+    idxAndTotal input
+    |> Seq.filter (fst >> (shouldMul <| allDosAndDonts input))
+    |> Seq.map snd
+    |> Seq.sum
+
+printfn $"%A{p2}"
