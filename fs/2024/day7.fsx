af8199d769f7f817936645f336378ed05396ddf6
dashaw92 <dashaw92@gmail.com>
2024-12-08 11:21:36 -0500

    2024 Day 7 Part 2

--- fs/2024/day7.fsx
+++ fs/2024/day7.fsx
@@ -4,7 +4,7 @@ open AoCShared
 open System
 
 type D7 = { res: int64; nums: int64 list}
-type Op = int -> int -> int
+type Op = int64 -> int64 -> int64
 
 let toDay7 (st: string) =
     let split = st.Split ':'
@@ -16,29 +16,53 @@ let toDay7 (st: string) =
         |> List.ofArray
     { res = res; nums = nums }
 
-let opers = [ (+); (*) ]
+let concat (a: int64) (b: int64) = int64 <| sprintf $"%d{a}%d{b}"
+let opers1 = [ (+); (*) ]
+let opers2 = [ (+); (*); concat ]
 let input = rf "day7.txt" |> lines |> List.ofArray |> List.map toDay7
 
 let genAll (ops: 'a list) n =
-    let toOps num =
-        [0..n - 1]
-        |> List.map (fun idx -> (num >>> idx) &&& 1)
-        |> List.map (function
-        | 0 -> ops[0]
-        | 1 -> ops[1]
-        | _ -> failwith "Uh?")
+    let toBaseStr b n =
+        if n = 0 then
+            "0"
+        else
+            let rec aux buf i =
+                if i <= 0 then
+                    buf
+                else
+                    let bufN = sprintf "%d%s" (i % b) buf
+                    aux bufN (i / b)
+            let out = aux "" n
+            out
+        
 
+    let padStr len (s: string) =
+        if s.Length >= len then
+            s
+        else
+            sprintf "%s%s" (String.replicate (len - s.Length) "0") s
+
+    let toOps (num: string) =
+        num.ToCharArray()
+        |> Array.map (fun c -> ops[int c - (int '0')])
+        |> List.ofArray
+    
     let m = List.length ops
+    let maxlen = (toBaseStr m ((pown m n) - 1)).Length
     [0..(pown m n) - 1]
+    |> List.map (toBaseStr m)
+    |> List.map (padStr maxlen)
     |> List.map toOps
 
-let table =
-    [2..12]
-    |> List.map (fun i -> i, genAll opers i)
+let genTable ops =
+    [1..12]
+    |> List.map (fun i -> i, genAll ops i)
     |> Map.ofList
 
-let getOpers n =
-    Option.get <| Map.tryFind n table
+let table1 = genTable opers1
+let table2 = genTable opers2
+
+let getOpers ops n = Option.get <| Map.tryFind n ops
 
 let rec collapse nums ops =
     match nums with
@@ -48,13 +72,16 @@ let rec collapse nums ops =
         collapse (res :: ns) (List.tail ops)
     | [] -> failwith "Bad input"
 
-let solveSingle d7 =
-    let ops = getOpers (d7.nums.Length - 1)
-    ops
+let solveSingle ops d7 =
+    getOpers ops (d7.nums.Length - 1)
     |> List.map (collapse d7.nums)
     |> List.exists (fun solved -> solved = d7.res)
 
-let solveP1 = List.filter solveSingle >> List.map _.res >> List.sum
+let solveP1 = List.filter (solveSingle table1) >> List.map _.res >> List.sum
+let solveP2 = List.filter (solveSingle table2) >> List.map _.res >> List.sum
 
 let p1 = solveP1 input
 printfn $"%d{p1}"
+
+let p2 = solveP2 input
+printfn $"%d{p2}"
