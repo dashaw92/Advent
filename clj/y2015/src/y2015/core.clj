055fc198aa3c48633953ace37201bd9371227f0b
dashaw92 <dashaw92@gmail.com>
2025-10-30 11:21:20 -0400

    update Clojure 2015 day 1

--- clj/y2015/src/y2015/core.clj
+++ clj/y2015/src/y2015/core.clj
@@ -1,5 +1,6 @@
 (ns y2015.core)
 
+;; filename: String -> char[]
 (defn get-input 
   "Read the file into a vector of characters"
   [file] 
@@ -7,25 +8,26 @@
        slurp
        vec))
 
-(defn final-floor 
-  [input]
-  (letfn [(step [input current] ;letfn gets rid of a warning "inline def" (defn)... 
-            (case (first input) 
-              \( (recur (rest input) (inc current))
-              \) (recur (rest input) (dec current))
-              nil current))]
-    (step input 0)))
-
-(defn find-first-basement
-  [input]
-  (letfn [(step [input current index]
-                (if (= current -1)
-                  index
-                  (case (first input)
-                    \( (recur (rest input) (inc current) (inc index))
-                    \) (recur (rest input) (dec current) (inc index)))))]
-    (step input 0 0)))
 
 (def input (get-input "resources/day1.txt"))
-(println (str "Part 1: " (final-floor input)))
-(println (str "Part 2: " (find-first-basement input)))
+
+;; Translates the input from the ( and ) "stairs" into absolute
+;; "up" (1) or "down" (-1) instructions which can be summed via reduce (or reductions)
+(defn remap-floors [input]
+  (map {\( 1 \) -1} input))
+
+;; Simple sum- final output is the final stair level Santa ends his journey on
+(def part1 (reduce + (remap-floors input)))
+;; reductions yields a new sequence where each index is replaced by the function's application
+;; as it traverses through the original sequence. With the `remap-floors` sequence,
+;; each step is a running accumulation- where Santa is at that index. From there, just
+;; find the first index of -1, or where Santa entered the basement levels for the first
+;; time.
+;; #(not= -1 %1) is a short-form anonymous function, and could be rewritten as:
+;; (fn [arg] (not= -1 arg)), but obviously #(...) is easier to read. %1 is the argument.
+;; The index found here from count'ing the result of take-while is further inc'ed because
+;; the problem wants the index of the first -1 and take-while is end-exclusive.
+(def part2 (inc (count (take-while #(not= -1 %1) (reductions + (remap-floors input))))))
+
+(println (str "Part 1: " part1))
+(println (str "Part 2: " part2))
