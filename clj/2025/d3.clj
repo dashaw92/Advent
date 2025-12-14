449887231865178831419150578a3b195ec4aff9
Daniel Shaw <dashaw92@gmail.com>
2025-12-14 14:34:29 -0500

    2025 Day 3 Part 2

--- clj/2025/d3.clj
+++ clj/2025/d3.clj
@@ -2,7 +2,7 @@
 ;; \0 -> 0
 ;; don't pass in non-numeric characters and expect anything useful
 (defn char-to-num [x]
-  (- (int x) (int \0)))
+  (- (long x) (long \0)))
 
 ;; Read f and split on newlines
 (defn lines [f]
@@ -12,36 +12,49 @@
 
 ;; Create all possible slices of the sequence that have a tail
 ;; Sort by the first element, find the slice which has the largest
-;; value first
-(defn largest-with-tail [bats]
-  (let [slices (for [x (range 2 (inc (count bats)))] (take-last x bats))
+;; value first. Split this slice into [first rest...]
+(defn largest-with-tail [min-size bats]
+  (let [slices (for [x (range min-size (inc (count bats)))] (take-last x bats))
         sorted (reverse (sort-by first slices))
         largest (first sorted)]
-    largest))
+    [(first largest) (rest largest)]))
 
-;; Find the next max value in a slice ignoring the first element
+;; Find the next max value in a slice. Terminal operation for this puzzle
+;; This edge case is required because largest-with-tail only yields slices
+;; of length > 1.
 (defn next-largest [slice]
-  (let [next-max (apply max (rest slice))]
+  (let [next-max (apply max slice)]
     next-max))
 
 ;; [1 3] -> 13
 (defn bats-to-num [[a b]]
   (+ (* a 10) b))
 
-;; Given a battery bank, find the largest two digit number that can be
+;; Given a battery bank, find the largest `iters`-digit number that can be
 ;; derived from the individual batteries in the bank
-;; 134982 -> 98
-(defn largest-bats [bats]
-  (let [first-max (largest-with-tail bats)
-        a (first first-max)
-        b (next-largest first-max)
+;; 134982:
+;;   iters = 2 -> 98
+;;   iters = 3 -> 982
+;;   iters = 4 -> 4982
+(defn largest-bats [iters bats]
+  (let [
+        [a remaining]
+        (loop
+            [acc 0
+             i (dec iters)
+             [a next-slice] (largest-with-tail iters bats)]
+          (if (= 1 i)
+            [(bats-to-num [acc a]) next-slice]
+            (recur (bats-to-num [acc a]) (dec i) (largest-with-tail i next-slice))))
+        b (next-largest remaining)
         out (bats-to-num [a b])]
     out))
 
-(defn solve-p1 []
-  (->> (lines "d3.txt")
+(defn solve [f iters]
+  (->> (lines f)
      (map #(map char-to-num %))
-     (map largest-bats)
+     (map (partial largest-bats iters))
      (reduce + 0)))
 
-(solve-p1)
+(solve "d3.txt" 2)
+(solve "d3.txt" 12)
