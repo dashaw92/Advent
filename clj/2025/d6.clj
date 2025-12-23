07f4046a69ab8bf2ec64247315bc7fa8feeec419
Daniel Shaw <dashaw92@gmail.com>
2025-12-23 17:05:21 -0500

    2025 Day 6 Part 2 almost

--- clj/2025/d6.clj
+++ clj/2025/d6.clj
@@ -23,12 +23,38 @@
         op ({"+" + "*" * "/" / "-" -} op)]
     {:nums nums :op op}))
 
-;; reduce each equation in the input using the op from each
-;; sum the results for part 1
-(defn solve-p1 [file]
-  (->>
-   (map to-p1 (lines file))
+(defn convert-p2 [record]
+  (let [nums (butlast record)
+        op (last record)
+        longest (map count nums)
+        how-many (apply max longest)
+        new-nums (for [i (range how-many)]
+                   (map #(nth % i nil) nums))
+        new-nums (map (partial apply str) new-nums)
+        output (reverse (conj new-nums op))]
+    (println output)
+    output))
+
+(defn solve [records]
+  (->> records
    (map #(apply (:op %) (:nums %)))
    (reduce + 0)))
 
-(solve-p1 "d6.txt")
+;; reduce each equation in the input using the op from each
+;; sum the results for part 1
+(defn solve-p1 [file]
+  (->> file
+   (lines)
+   (map to-p1)
+   (solve)))
+
+(defn solve-p2 [file]
+  (->> file
+   (lines)
+   (map convert-p2)
+   (map to-p1)
+   (solve)))
+
+(solve-p1 "d6e.txt")
+;; this almost works except the alignment matters. this solution always assumes left alignment which throws off the answer by a ton.
+(solve-p2 "d6e.txt")
