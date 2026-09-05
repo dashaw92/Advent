38e3d0236c667f1564d5fd3b37fba0e8dbcc48ea
dashaw92 <dashaw92@gmail.com>
2026-09-05 05:19:58 -0400

    Work on 2025 day 11

--- clj/src/y2025/day11.clj
+++ clj/src/y2025/day11.clj
@@ -16,7 +16,27 @@
         (map line->node $)
         (reduce #(assoc %1 (:node %2) (:conns %2)) {} $)))
 
+(defn terminal? [nodes node]
+  (not (contains? nodes node)))
+
+(defn routes [nodes start] :TODO)
+;(defn routes [nodes start]
+;  (reduce +
+;          (letfn [(aux [nodes start route]
+;                    (let [conns (nodes start)
+;                          route (cons start route)
+;                          all-routes
+;                          (for [conn conns]
+;                            (if (terminal? nodes conn)
+;                              (cons conn route)
+;                              (aux nodes conn route)))
+;                          counts (mapv count all-routes)]
+;                      counts))]
+;            (aux nodes start []))))
+
 (def example-input "src/y2025/d11e.txt")
 (def example (lines example-input))
+(def input-path "src/y2025/d11.txt")
+(def input (lines input-path))
 
-
+(routes input :you)
