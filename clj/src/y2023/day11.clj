7e056c79d0401e107b35b1565e4f015b0b067245
dashaw92 <dashaw92@gmail.com>
2026-09-06 11:57:30 -0400

    Refactor logic into expansion function to reduce work required

--- clj/src/y2023/day11.clj
+++ clj/src/y2023/day11.clj
@@ -19,29 +19,20 @@
       (for [row (->rows grid)]
         (nth row y)))))
 
-(defn empty-indices [grid dimension-extractor]
-  (let [lines (dimension-extractor grid)
-        empty-lines (map all-empty? lines)
-        idx-pred (fn [idx bool-item] (if bool-item idx nil))
-        idxs (keep-indexed idx-pred empty-lines)]
-    idxs))
-
-(defn expand-via-empty [lines empty]
-  (loop [i 0
-         empty empty
-         lines lines
+(defn expand-via-empty [lines]
+  (loop [lines lines
          buffer []]
     (let [current-line (first lines)
           lines (rest lines)]
       (if (nil? current-line)
         buffer
-        (if (= i (first empty))
-          (recur (inc i) (rest empty) lines (concat buffer [current-line current-line]))
-          (recur (inc i) empty lines (concat buffer [current-line])))))))
+        (if (all-empty? current-line)
+          (recur lines (concat buffer (repeat 2 current-line)))
+          (recur lines (concat buffer [current-line])))))))
 
 (defn expand-all [input]
-  (let [rows-expanded (expand-via-empty (->rows input) (empty-indices input ->rows))
-        cols-expanded (expand-via-empty (->cols rows-expanded) (empty-indices rows-expanded ->cols))
+  (let [rows-expanded (expand-via-empty (->rows input))
+        cols-expanded (expand-via-empty (->cols rows-expanded))
         transposed-to-normal (apply map vector cols-expanded)]
     transposed-to-normal))
 
