360f2fad5ff0acbabc349635e7cb74fa6e24a033
dashaw92 <dashaw92@gmail.com>
2026-09-14 10:07:43 -0400

    2019 Day 2 Part 2 Clojure

--- clj/src/y2019/day2.clj
+++ clj/src/y2019/day2.clj
@@ -7,7 +7,7 @@
   (as-> f $
         (slurp $)
         (str/split $ #",")
-        (map Integer/parseInt $)
+        (map ^[String] Integer/parseInt $)
         (assoc {} :tape (vec $))))
 
 ;Functions for Intcode opcodes take 3 arguments:
@@ -82,14 +82,14 @@
     (let [next (first cell->values)
           rest (rest cell->values)
           next-state (assoc-in state [:tape (:cell next)] (:val next))]
-      (if (empty? cell->values)                             ;note: checking `rest` here would cause the final map to be ignored
-        state
+      (if (empty? rest)
+        next-state
         (recur next-state rest)))))
 
-(defn solve-p1
-  "Part 1 requires you to overwrite the values at cells 1 and 2 in the input
+(defn solve
+  "This puzzle requires you to overwrite the values at cells 1 and 2 in the input
   tape to specific values. Pass the map of overwrites as a vec of maps with
-  keys :cell and :val corresponding to the index and value to set. Afterward,
+  keys :cell and :val corresponding to the index and value to set. Afterwards,
   return the value of the cell at position 0 in the tape."
   [f overwrites]
   (let [init-state (read-state f)
@@ -98,4 +98,20 @@
         pos0 (first (:tape output-state))]
     pos0))
 
-(solve-p1 "src/y2019/d2.txt" [{:cell 1 :val 12} {:cell 2 :val 2}])
+(def p1 (solve "src/y2019/d2.txt" [{:cell 1 :val 12} {:cell 2 :val 2}]))
+(def p2
+  (let [state (read-state "src/y2019/d2.txt")]
+    (loop [noun 0
+           verb 0]
+      (let [modded-state (overwrite-cells state [{:cell 1 :val noun} {:cell 2 :val verb}])
+            output (run modded-state)
+            addr0 (first (:tape output))]
+        (if (= 19690720 addr0)
+          (+ (* noun 100) verb)
+          (if (and (> noun 99) (> verb 99))
+            (throw (IllegalStateException. "Exceeded limits for noun and verb (>99)."))
+            (if (> noun 99)
+              (recur 0 (inc verb))
+              (recur (inc noun) verb))))))))
+
+(println (str "Part 1: " p1 "\nPart 2: " p2))
