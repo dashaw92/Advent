dbe0799af58c5e941235aa6d63260ffde5b5121d
dashaw92 <dashaw92@gmail.com>
2026-09-14 11:26:42 -0400

    2019 Day 5 Part 1 solved (Clojure) Parameter modes are handled by the opcode because parameter usage is not uniform across all. Output arguments (arguments used to write values) are ALWAYS in mode "0" (positional), and some opcodes only have these types (read/write opcodes) while the binops use the first two parameter modes and ignore the last.

--- clj/src/y2019/day5.clj
+++ clj/src/y2019/day5.clj
@@ -18,9 +18,23 @@
            lit->state))
   ([f input] (bind-input (read-state f) input)))
 
+(defn read-addr
+  "Handles reading from the tape with support for all argument modes."
+  [tape mode addr]
+  (case mode
+    :position (nth tape addr)
+    :immediate addr))
 
-;Functions for Intcode opcodes take 2 arguments:
-;[[args...] state]
+(defn extract-arg-modes [opcode]
+  (let [base-opcode (mod opcode 100)
+        params (take-last 3 (concat (repeat 3 \0) (drop-last 2 (str opcode))))
+        to-mode #(if (= \0 %) :position :immediate)
+        params (map to-mode params)]
+    {:opcode base-opcode :params (reverse params)}))
+
+;Functions for Intcode opcodes take 3 arguments:
+;[[param0 param1 param2] [args...] state]
+;paramN = mode of parameter at index N in args (:position or :immediate)
 ;args is a variable length vec of arguments required by the opcode
 ;state is the entire state map with the program tape in :tape
 ;The function should yield the next state as a map with the new tape at :tape (assoc state :tape (modify (:tape state)))
@@ -28,23 +42,23 @@
 (defn binop
   "Binary operations where the only code that changes between opcodes is the function applied to
   the operands. The state map will be updated with the result of the operation provided."
-  [op [in1 in2 out] state]
+  [op [param1 param2 _] [in1 in2 out] state]
   (let [tape (:tape state)
-        in1 (nth tape in1)
-        in2 (nth tape in2)
+        in1 (read-addr tape param1 in1)
+        in2 (read-addr tape param2 in2)
         result (op in1 in2)
         tape' (assoc tape out result)]
     (assoc state :tape tape')))
 
 (defn halt
-  "Ignores the first argument to match the shape of opcode functions.
+  "Ignores the first two arguments to match the shape of opcode functions.
   Adds the :halted flag to the state to indicate the program has finished."
-  [_ state] (assoc state :halted true))
+  [_ _ state] (assoc state :halted true))
 
 (defn read-in
   "'Reads' a single integer and stores it at [`addr`]. Input is popped from
   the front of the :input vec in state."
-  [[addr] state]
+  [_ [addr] state]
   (let [input (first (:input state))
         rest (rest (:input state))
         replaced-state (assoc-in state [:tape addr] input)]
@@ -53,8 +67,8 @@
 
 (defn write-out
   "Outputs the value at the address (pushes it onto the :output stack)"
-  [[addr] state]
-  (let [val (nth (:tape state) addr)]
+  [[param0 _ _] [addr] state]
+  (let [val (read-addr (:tape state) param0 addr)]
     (assoc state :output (cons val (:output state)))))
 
 (def opcodes
@@ -73,18 +87,19 @@
   [state pc]
   (let [tape (:tape state)
         current (nth tape pc nil)
+        {:keys [opcode params]} (extract-arg-modes current)
         rest (drop (inc pc) tape)
-        args (take (:len (opcodes current)) rest)]
-    (if (not (contains? opcodes current))
-      (throw (IllegalArgumentException. (str "Invalid opcode " current)))
-      (cons current args))))
+        args (take (:len (opcodes opcode)) rest)]
+    (if (not (contains? opcodes opcode))
+      (throw (IllegalArgumentException. (str "Invalid opcode " opcode)))
+      (cons [opcode params] args))))
 
 (defn run-step
   "Execute a single step of the Intcode program: state -> state'"
-  [[opcode & args] state]
+  [[[opcode params] & args] state]
   (let [args (vec args)
         func (:func (opcodes opcode))]
-    (func args state)))
+    (func params args state)))
 
 (defn run
   "Execute the Intcode program until it either halts (via :halted) or runs out of instructions (pc > count).
@@ -101,28 +116,12 @@
           state
           (recur (+ pc (count instrs)) (run-step instrs state)))))))
 
-(defn overwrite-cells
-  "Overwrite cells at :cell x with :val y (via vec of maps)."
-  [state cell->values]
-  (loop [state state
-         cell->values cell->values]
-    (let [next (first cell->values)
-          rest (rest cell->values)
-          next-state (assoc-in state [:tape (:cell next)] (:val next))]
-      (if (empty? rest)
-        next-state
-        (recur next-state rest)))))
-
-;(defn solve
-;  "This puzzle requires you to overwrite the values at cells 1 and 2 in the input
-;  tape to specific values. Pass the map of overwrites as a vec of maps with
-;  keys :cell and :val corresponding to the index and value to set. Afterwards,
-;  return the value of the cell at position 0 in the tape."
-;  [f overwrites]
-;  (let [init-state (read-state f)
-;        overwritten-state (overwrite-cells init-state overwrites)
-;        output-state (run overwritten-state)
-;        pos0 (first (:tape output-state))]
-;    pos0))
-
-(run (lit->state "3,0,4,0,99" [0]))
+(run (lit->state "1002,4,3,4,33" [0])) ;correctly halts {:tape [1002 4 3 4 99] :halted true ..}
+(def p1
+  (let [state (run (read-state "src/y2019/d5.txt" [1]))
+        all-passed (every? zero? (rest (:output state)))
+        output0 (first (:output state))]
+    (if all-passed
+      (println (str "Passed all diagnostic tests. Part 1 is " output0))
+      (println "Did not pass diagnostic tests."))
+    output0))
